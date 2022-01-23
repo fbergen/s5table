fdd42b0a0eb6cf929e1f0813ed8f107bbcabe8bc
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2022-01-23 22:49:57 +0100

    fix imports

--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,63 @@
-extern crate leveldb;
+extern crate rocket;
 
-extern crate db_key as key;
-use crate::leveldb::compaction::Compaction;
-use crate::leveldb::iterator::Iterable;
-use leveldb::database::Database;
-use leveldb::kv::KV;
-use leveldb::options::{Options, ReadOptions, WriteOptions};
-use serde::{Deserialize, Serialize};
-use sstable::*;
-use std::fs;
-use std::io::Read;
-use std::path::Path;
-use std::path::PathBuf;
+use rocket::form::FromForm;
+use rocket::http::Method;
+use rocket::{get, launch, routes, State};
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use sstable::filter;
 use std::sync::Arc;
 use tokio::task;
 
-mod s3;
-use crate::s3::S3File;
-mod gcs;
-use crate::gcs::GCSFile;
+extern crate s5table;
+#[allow(unused_imports)]
+use s5table::gcs::GCSFile;
+#[allow(unused_imports)]
+use s5table::s3::S3File;
 
-use db_key::Key;
-
-#[derive(Debug)]
-struct MyKey(String);
-
-impl MyKey {
-    fn new(s: &str) -> MyKey {
-        MyKey(s.to_string())
-    }
+#[derive(FromForm, Debug)]
+struct GetParam {
+    key: Option<String>,
 }
 
-impl Key for MyKey {
-    fn from_u8(key: &[u8]) -> MyKey {
-        MyKey(std::str::from_utf8(key).unwrap().to_string())
-    }
+#[get("/get?<params..>")]
+async fn get(table: &State<sstable::Table>, params: GetParam) -> Option<Vec<u8>> {
+    let k = params.key?;
+    println!("Get key {:?}", k);
 
-    fn as_slice<T, F: Fn(&[u8]) -> T>(&self, f: F) -> T {
-        f(self.0.as_bytes())
-    }
+    task::block_in_place(move || table.get(k.as_bytes())).unwrap()
 }
 
-#[tokio::main]
-async fn main() {
-    // let path = PathBuf::from("./random.sstable");
-    // write_table(&path);
-    // write_test_db_locally();
-    // read_db_from_s3().await;
-    read_db().await;
-}
+#[launch]
+async fn rocket() -> _ {
+    println!("Reading sstable file");
+    // let file_location = env::var("SSTABLE_FILE").unwrap().expect("Need to set SSTABLE_FILE env variable");
 
-fn read_table(file: Box<dyn RandomAccess>, len: usize) -> Result<sstable::Table> {
-    use std::time::Instant;
-    let start = Instant::now();
-    println!("Getting sstable file");
+    let file = GCSFile::new("githope-eu", "experimental/test.sstable").await;
+    // let file = S3File::new("flyvc", "fberge/test.sstable").await;
+    println!("Done Readng sstable file");
+    let len = file.len as usize;
     let mut options = sstable::Options::default();
     options.filter_policy = Arc::new(Box::new(filter::NoFilterPolicy::new()));
-    let tr = sstable::Table::new(options, file, len)?;
 
-    println!("Startup: {}µs", start.elapsed().as_micros());
-    let start = Instant::now();
+    let table =
+        task::spawn_blocking(move || sstable::Table::new(options, Box::new(file), len).unwrap())
+            .await
+            .unwrap();
 
-    let k = format!("{:010}", 3943243);
-    let k = "google/leveldb";
-    println!("Get key");
-    let res = tr.get(k.as_bytes());
+    let allowed_origins = AllowedOrigins::all();
 
-    println!("{:?}", &res);
-    println!("Read: {}µs", start.elapsed().as_micros());
-    // let mut iter = tr.iter();
-    // while iter.advance() {
-    //     let (k, v) = sstable::current_key_val(&iter).unwrap();
-    //     println!(
-    //         "{} => {}",
-    //         String::from_utf8(k).unwrap(),
-    //         String::from_utf8(v).unwrap()
-    //     );
-    // }
-    Ok(tr)
-}
-
-async fn read_db() {
-    let _path = PathBuf::from("./test_db/000034.ldb");
-
-    //let f = S3File::new("flyvc", "fberge/000034.ldb").await;
-    let _f = S3File::new("flyvc", "fberge/random.sstable").await;
-
-    let f = GCSFile::new("githope-eu", "experimental/test.sstable").await;
-    // let f = GCSFile::new("githope-eu", "empty.json").await;
-
-    let mut buffer = [0; 10];
-    println!("{:?}", f.async_read_at(0, &mut buffer).await);
-
-    // let f = S3File::new("flyvc", "fberge/test.sstable").await;
-
-    let res = task::spawn_blocking(move || {
-        let l = f.len as usize;
-
-        let tr = read_table(Box::new(f), l).expect("Reading the table failed");
-    })
-    .await;
-}
-
-async fn read_db_from_s3() {
-    //let mut f = S3File::new("flyvc", "fberge/newcompanies.jsonl").await;
-    let mut f = S3File::new("flyvc", "fberge/db_structure_company.sql").await;
-
-    let mut buffer = [0; 10];
-
-    let n = f.read(&mut buffer[..]).unwrap();
-    println!(
-        "The bytes: {:?}",
-        std::str::from_utf8(&buffer[..n]).unwrap()
-    );
-}
-
-fn write_table(p: &Path) -> Result<()> {
-    let dst = fs::OpenOptions::new()
-        .create(true)
-        .truncate(true)
-        .write(true)
-        .open(p)?;
-    let mut tb = sstable::TableBuilder::new(sstable::Options::default(), dst);
-
-    for i in 0..10000000 {
-        let k = format!("{:010}", i);
-        let v = format!("{}value", i);
-        tb.add(&k.as_bytes().to_vec(), &v.as_bytes().to_vec())?;
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
     }
+    .to_cors()
+    .expect("Valid cors");
 
-    tb.finish()?;
-    Ok(())
-}
-
-fn write_test_db_locally() {
-    let path = Path::new("./test_db2");
-    println!("writing to {:?}", path);
-
-    let mut options = Options::new();
-    options.create_if_missing = true;
-    let database: Database<MyKey> = match Database::open(path, options) {
-        Ok(db) => db,
-        Err(e) => {
-            panic!("failed to open database: {:?}", e)
-        }
-    };
-
-    let write_opts = WriteOptions::new();
-    for i in 0..10000000 {
-        let k = format!("{}", i);
-        let v = format!("{}value", i);
-        match database.put(write_opts, MyKey(k), v.as_bytes()) {
-            Ok(_) => (),
-            Err(e) => {
-                panic!("failed to write to database: {:?}", e)
-            }
-        };
-    }
-
-    println!("compacting...");
-    database.compact(&MyKey::new("0"), &MyKey::new("AAAAAAAAAAAAA"));
-
-    database
-        .iter(ReadOptions::new())
-        .for_each(|(k, v)| println!("{:?}", (k, v)));
-
-    let read_opts = ReadOptions::new();
-    let res = database.get(read_opts, &MyKey::new("1"));
-
-    match res {
-        Ok(data) => {
-            assert!(data.is_some());
-            assert_eq!(data, Some(b"1value".to_vec()));
-        }
-        Err(e) => {
-            panic!("failed reading data: {:?}", e)
-        }
-    }
-
-    //let t = Table::open("./test_db/DBCOPY.ldb");
-    //println!("{:?}", t.get("1"));
+    rocket::build()
+        .attach(cors)
+        .manage(table)
+        .mount("/", routes![get])
 }
